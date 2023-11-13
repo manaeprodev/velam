574e9fec20a959b886f663a71d4ef8fc79c4c1b5
Tony TIRYAKI <ttiryaki@mediametrie.fr>
2023-11-13 17:44:11 +0100

    add direct & dataflow runners

--- beam/DataflowRunner/main.py
+++ beam/DataflowRunner/main.py
@@ -1,10 +1,13 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io.gcp.internal.clients import bigquery
+from apache_beam.io.gcp import bigquery
 import json
 
 beam_options = PipelineOptions(
-    runner='DirectRunner'
+    runner="DataflowRunner",
+    project='velam-402208',
+    job_name="velam-bigquery",
+    region="europe-west9",
 )
 
 table_spec = bigquery.TableReference(
@@ -14,7 +17,11 @@ table_spec = bigquery.TableReference(
 
 table_schema = {
     'fields': [
-        {'name': 'nom', 'type': 'STRING', 'mode': 'NULLABLE'}
+        {'name': 'nom_station', 'type': 'STRING', 'mode': 'NULLABLE'},
+        {'name': 'porte_velo', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'porte_velo_dispo', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'velo_dispo', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+        {'name': 'derniere_maj', 'type': 'INTEGER', 'mode': 'NULLABLE'}
     ]
 }
 
@@ -24,12 +31,17 @@ class StationName(beam.DoFn):
         return [station for station in stations]
 
 
-with beam.Pipeline() as p:
-    (p | "Read" >> beam.io.ReadFromText('gs://ma_data_velam_bucket/reponse_api_20231016160001.json')
+with beam.Pipeline(options=beam_options) as p:
+    (p | "Read" >> beam.io.ReadFromText('gs://ma_data_velam_bucket/')
      | "Lit les données" >> beam.Map(lambda line: json.loads(line))
      | "Split les stations" >> beam.ParDo(StationName())
-     | "Nom des stations" >> beam.Map(lambda station: station['name'] if station['name'] is not None else '')
-     | "Mise en forme" >> beam.Map(lambda nom: {'nom': nom})
+     | "Nom des stations" >> beam.Map(lambda station: {
+                "nom_station": station.get("name", ""),
+                "porte_velo": station.get("bike_stands", None),
+                "porte_velo_dispo": station.get("available_bike_stands", None),
+                "velo_dispo": station.get("available_bikes", None),
+                "derniere_maj": station.get("last_update", "")
+            })
      | "Enregistre les données" >> beam.io.WriteToBigQuery(table_spec,
                                                            schema=table_schema,
                                                            custom_gcs_temp_location='gs://velam-collection-test',
